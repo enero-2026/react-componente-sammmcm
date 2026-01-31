5e87fc3b7b23b517708b59b8a0313693949ce484
Sam <samicandelaria@gmail.com>
2026-01-31 17:06:59 -0600

    Ejercicio 21 de Enero. Samantha Candelaria Mora.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="hola mundo"></Button>
       <StatusBar style="auto" />
     </View>
   );
